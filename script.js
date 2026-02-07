0317616df30a323bd3207343f1f83a90d319ce7e
MdMozakkeAnam <129014517+MdMozakkeAnam@users.noreply.github.com>
2026-02-07 22:41:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstWord(s) {
   // your code here
+	if (str.length === 0) return '';
+    
+    let spaceIndex = str.indexOf(' ');
+    if (spaceIndex === -1) {
+        return str;
+    }
 }
 
 // Do not change the code below
